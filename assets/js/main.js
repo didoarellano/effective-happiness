49a253651b51c8cd0a27688079e39c4ee399eee1
Dido Arellano <dido@semicolonstudios.com>
2017-03-17 09:15:14 +0800

    please make the logo bigger

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,28 @@
     $('.tagline > *').slabText()
   }
 
-  function navBar() {
+  function navBar () {
     var navbar = document.querySelector('.main-nav')
-    var waypoint = new Waypoint({
+
+    new Waypoint({
       element: document.querySelector('.about'),
-      offset: 60,
-      handler: function () {
-        navbar.classList.toggle('has-background')
+      offset: 80,
+      handler: function (direction) {
+        navbar.classList.toggle('past-threshold')
+
+        // IE10/11 doesn't support toggle's 2nd arg
+        if (direction === 'up') {
+          navbar.classList.add('transition')
+        } else {
+          navbar.classList.remove('transition')
+        }
       }
     })
+
+    var headroom = new Headroom(navbar)
+    headroom.init()
   }
 
   slabText()
   navBar()
-
 }())
